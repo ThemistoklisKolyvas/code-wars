b3f751ad2b9d87acf52218bdb081e6703f93a4c1
Themistoklis Kolyvas <themiskolivas@hotmail.com>
2024-12-13 23:37:09 +0200

    Lost with out a map addition

---
+++ LostWithoutaMap.py
@@ -0,0 +1,10 @@
+# Given an array of integers, return a new array with each value doubled.
+
+# For example:
+
+# [1, 2, 3] --> [2, 4, 6]
+# my solution:
+def maps(n):
+    for i in range(len(n)):  
+        n [i] = n[i] * 2      
+    return n   
